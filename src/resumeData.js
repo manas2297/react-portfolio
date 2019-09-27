86c37e2287cee128c294daac34aa4e1d38dbed93
Kiran Kulkarni <kiran.kulkarni@cronj.com>
2019-09-27 22:28:56 +0530

    portfolio initia

---
+++ src/resumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+  imagebaseurl: "",
+  name: "Divyanshi Singh",
+  role: "Web developer | Coder | Machine Learning | Writer",
+  linkedinId: "https://www.linkedin.com/in/Divyanshi",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "I am a self taught web developer and Data Scientist, who loves to code and learn new technologies. Writing is my art!!!",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/divyanshipilot",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "http://github.com/divyanshipilot",
+      className: "fa fa-github"
+    },
+    {
+      name: "skype",
+      url: "",
+      className: "fa fa-twitter"
+    }
+  ],
+  aboutme:
+    "I am currently a B.Tech 2nd Year student in Madan Mohan Malaviya University of Technology, Gorakhpur. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  address: "Bariely,India",
+  website: "https://divyanshi.github.io",
+  education: [
+    {
+      UniversityName: "Madan Mohan Malaviya University Of Technology",
+      specialization: "Computer Science & Engineering",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2022",
+      Achievements:
+        "State level Takewando Champion | NCC Cadet | Best Youth Parliament Speaker"
+    }
+  ],
+  work: [
+    {
+      CompanyName: "Student",
+      specialization: "Looking for Internships",
+      MonthOfLeaving: "December",
+      YearOfLeaving: "2019",
+      Achievements: "Machine Learning | Web Developer"
+    }
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5"
+    },
+    {
+      skillname: "CSS"
+    },
+    {
+      skillname: "Python3"
+    },
+    {
+      skillname: "c++"
+    }
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "./images/portfolio/origami.jpg"
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/coffee.jpg"
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/judah.jpg"
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/retrocam.jpg"
+    }
+  ],
+  testimonials: [
+    {
+      description:
+        "She is hardworking and really good in python and machine learning.",
+      name: "Manas Yadav"
+    },
+    {
+      description: "She has excellent Technical skills in machine learning ",
+      name: "Shivam Kumar"
+    }
+  ]
+};
+
+export default resumeData;
